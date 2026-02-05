ef86c2dbe2b009df44c1bbafe46f09655dcbfc16
Jimena <mjimevm@gmail.com>
2026-02-04 19:59:53 -0600

    Minor changes in random Numbers

--- demo/src/main/java/org/sorting/RandomNumbersGenerator.java
+++ demo/src/main/java/org/sorting/RandomNumbersGenerator.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class RandomNumbersGenerator {
     public ArrayList<Integer> generateRandomNumbers(int size) {
+        int maxDigits = 4; // 4 digits
         if (size <= 0) {
             throw new IllegalArgumentException("Size must be a positive integer.");
         } else {
@@ -15,8 +16,8 @@ public class RandomNumbersGenerator {
         Random rand = new Random();
         ArrayList<Integer> numbers = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
-            numbers.add(rand.nextInt());
-        }
+            numbers.add(rand.nextInt((int) Math.pow(10, maxDigits)));
+            }
         return numbers;
     }
 }
--- demo/src/main/java/org/sorting/Sorter.java
+++ demo/src/main/java/org/sorting/Sorter.java
@@ -1,6 +1,9 @@
 package org.sorting;
 public class Sorter<T extends Comparable<T>> {
     private T[] input;
+    public Sorter(T[] input) {
+        this.input = input;
+    }
     public T[] gnomeSort() {
         int index = 0;
         while (index < input.length) {
